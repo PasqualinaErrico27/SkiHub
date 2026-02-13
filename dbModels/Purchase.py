bee45b49ddc10b99412f79952311bb430e5813aa
PASQUALINA ERRICO <116086146+PasqualinaErrico27@users.noreply.github.com>
2026-02-13 23:22:43 +0100

    Update Purchase.py

--- dbModels/Purchase.py
+++ dbModels/Purchase.py
@@ -1,11 +1,16 @@
 from dbModels.db import db
 
+
 class Purchase(db.Model):
     __tablename__ = "purchase"
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    resort_id = db.Column(db.Integer, db.ForeignKey("resorts.id"))
+    resort_id = db.Column(db.Integer, db.ForeignKey("resorts.id"))  # Questo è l'ID (es. 1, 2, 3)
+
+    # AGGIUNGI QUESTA RIGA:
+    resort = db.relationship("Resort", backref="purchases")
+
     datefrom = db.Column(db.DateTime)
     dateto = db.Column(db.DateTime)
     count = db.Column(db.Integer)
